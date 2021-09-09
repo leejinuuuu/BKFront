fe2b70779280ea5c6a18c17ca6a73b2264bf8b52
jeongjin984 <philipwq3963@gmail.com>
2021-09-09 18:58:31 +0900

    m

--- pages/_app.js
+++ pages/_app.js
@@ -7,8 +7,8 @@ import {GET_LOGIN_PLATFORM_REQUEST} from "../config/event/eventName/userEvent";
 import wrapper from '../store/store-wrapper'
 
 function str_obj(str) {
-    str = str.split('; ');
     let result = {};
+    str = str.split('; ');
     for (let i = 0; i < str.length; i++) {
         let cur = str[i].split('=');
         result[cur[0]] = cur[1];
@@ -19,7 +19,10 @@ function str_obj(str) {
 class MyApp extends App {
     static getInitialProps = wrapper.getInitialAppProps(store => async ({Component, ctx}) => {
 
-        if(!ctx.req.url.includes("login")) {
+        if(
+            !ctx.req.url.includes("login") &&
+            !ctx.req.url.includes("signup")
+        ) {
             store.dispatch({
                 type: GET_LOGIN_PLATFORM_REQUEST,
                 data: str_obj(ctx.req.headers.cookie)["platform"]
