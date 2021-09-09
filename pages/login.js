c4909c1f82bd313ca78da42a7a88487918d53cb4
jeongjin <philipwq3963@gmail.com>
2021-09-09 13:51:58 +0900

    m

--- pages/login.js
+++ pages/login.js
@@ -3,13 +3,9 @@ import {Button, Form} from "react-bootstrap";
 import {useDispatch} from "react-redux";
 import {LOGIN_REQUEST} from "../config/event/eventName/userEvent";
 import Link from 'next/link'
-
-import getFormInput from "../utils/getFormInput";
-import {useRouter} from "next/router";
-
 const login = () => {
     const googleURL = "https://accounts.google.com/o/oauth2/v2/auth?" +
-        "client_id=512265054010-g3t0difpimrb59jr33dqld08gqkr7ijf.apps.googleusercontent.com&" +
+        "client_id=512265054010-31avv85pu1ufi1jbrd9vu3s5kj1006pa.apps.googleusercontent.com&" +
         "redirect_uri=http://localhost:8081/auth&" +
         "response_type=code&" +
         "scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile&" +
@@ -17,30 +13,25 @@ const login = () => {
     ;
 
     const dispatch = useDispatch();
-    const router = useRouter();
-
-    const [username, onChangeUsername] = getFormInput();
-    const [password, onChangPassword] = getFormInput();
 
     const handleSubmit = useCallback(e => {
         e.preventDefault();
-        console.log(username)
         const data = {
-            username: username,
-            password: password
+            username: e.target.querySelector("#formBasicEmail").value,
+            password:e.target.querySelector("#formBasicPassword").value
         }
         dispatch({
             type: LOGIN_REQUEST,
             data
         })
-    }, [username, password])
+    }, [])
 
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" onChange={onChangeUsername}/>
+                    <Form.Control type="text" placeholder="Enter email"/>
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -48,12 +39,12 @@ const login = () => {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" onChange={onChangPassword}/>
+                    <Form.Control type="password" placeholder="Password"/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                <Button variant="primary" type="submit">
                     Submit
                 </Button>
                 <Link href={googleURL}><Button variant="primary">Google</Button></Link>
--- saga/user-saga.js
+++ saga/user-saga.js
@@ -2,13 +2,16 @@ import {all, fork} from 'redux-saga/effects';
 
 import createSaga from "../utils/createSaga";
 
-const watchLogin = createSaga("LOGIN", "http://localhost:8081/login", "post")
+const watchLogin = createSaga("LOGIN", "http://localhost:8081/login", "post");
 
-const watchLoadGoogleProfile = createSaga("GOOGLE_LOAD_USER", "http://localhost:8081/user", "get")
+const watchSignup  = createSaga("SIGNUP", "http://localhost:8081/signup", "post");
+
+const watchLoadGoogleProfile = createSaga("GOOGLE_LOAD_USER", "http://localhost:8081/user", "get");
 
 export default function* userSaga() {
     yield all([
         fork(watchLogin),
-        fork(watchLoadGoogleProfile)
+        fork(watchLoadGoogleProfile),
+        fork(watchSignup)
     ]);
 }
