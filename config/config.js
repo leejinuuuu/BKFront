428312d44dc7df9c5bda9a458d922e4857e4848a
JeongJin984 <philipwq3963@gmail.com>
2021-11-29 13:11:18 +0900

    m

--- config/config.js
+++ config/config.js
@@ -1,2 +1,2 @@
-export const backURL = process.env.NODE_ENV === "development" ? "http://localhost:8081" : "http://amadeuos01.cafe24.com";
+export const backURL = process.env.NODE_ENV === "development" ? "http://localhost:8081" : "https://peniyo.co.kr";
 export const imageURL = backURL + "/image/";
