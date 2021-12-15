bdbf4a563518ed490901fe6809b1968248e4294d
JeongJin984 <philipwq3963@gmail.com>
2021-12-15 22:01:41 +0900

    m

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,14 +4,25 @@ import Providers from "next-auth/providers"
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    Providers.Google({
-      clientId: "512265054010-k3le1p2l9vldmthtnr7e5ev6cvh8icf3.apps.googleusercontent.com",
-      clientSecret: "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",
-      authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth?" +
-        "prompt=consent&" +
-        "response_type=code&" +
-        "access_type=offline"
-    }),
+    Providers.Google(
+      process.env.NODE_ENV === "development" ?
+        {
+          clientId: "512265054010-k3le1p2l9vldmthtnr7e5ev6cvh8icf3.apps.googleusercontent.com",
+          clientSecret: "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",
+          authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth?" +
+            "prompt=consent&" +
+            "response_type=code&" +
+            "access_type=offline"
+        } :
+        {
+          clientId: "135010180751-c2f9jdd7hkgkk0as4kunorbkm51n7j2b.apps.googleusercontent.com",
+          clientSecret: "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",
+          authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth?" +
+            "prompt=consent&" +
+            "response_type=code&" +
+            "access_type=offline"
+        }
+    ),
     // ...add more providers here
   ],
   jwt: {
